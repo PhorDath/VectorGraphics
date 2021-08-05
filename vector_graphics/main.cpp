c5804b325a864b01e23cad51239f27df1f5115b7
Victor Hugo Vidigal Corrêa <phordath@hotmail.com>
2021-08-05 18:47:51 -0300

    scalling added

--- vector_graphics/main.cpp
+++ vector_graphics/main.cpp
@@ -28,13 +28,17 @@ struct Model {
 
 struct Object {
 	Model* model;
-	float x, y;
+	Model tModel;
+	float x, y, scale = 1.0f, angle = 0.0f;
 
 	Object() {}
-	Object(Model* m, float x, float y) {
-		this->model = m;
+	Object(Model* model, float x, float y, float scale, float angle) {
+		this->model = model;
+		this->tModel = *model;
 		this->x = x;
 		this->y = y;
+		this->scale = scale;
+		this->angle = angle;
 	}
 };
 
@@ -45,62 +49,72 @@ public:
 	VectorGraphic()
 	{
 		sAppName = "VectorGraphic";		
-		vModels.push_back(Model({ Point(1, 4), Point(3, 4), Point(2, 2) }));
 	}
 
 private:
 	vector<Model> vModels;
+	vector<Object> vObjects;
 
 	bool OnUserCreate() override
 	{
-		// Called once at the start, so create things here
+		vModels.push_back(Model({ Point(1, 4), Point(3, 4), Point(2, 2) })); // add an isosceles triangle model in the application		
+		Object O1(&vModels[0], ScreenWidth() / 2, ScreenHeight() / 2, 5, 90); // create an object with the triangle model and set its position in the middle of the screen
+		vObjects.push_back(O1);
 
 		return true;
 	}
 
 	bool OnUserUpdate(float fElapsedTime) override
 	{
-		// called once per frame
-		Clear(olc::WHITE);
+		Clear(olc::WHITE); // clear screen
 
-		for (Model m : vModels) {
-			for (Point p1 : m.vPoints) {
-				for (Point p2 : m.vPoints) {
-					DrawLine(p1.dX, p1.dY, p2.dX, p2.dY, olc::BLACK);
-				}				
+
+		// render objects
+		for (Object O : vObjects) {
+			// translate
+
+
+			// scale
+			for (int i = 0; i < vObjects[0].model->vPoints.size(); i++) {
+				vObjects[0].tModel.vPoints[i].dX = vObjects[0].model->vPoints[i].dX * O.scale;// *fElapsedTime;
+				vObjects[0].tModel.vPoints[i].dY = vObjects[0].model->vPoints[i].dY * O.scale;// *fElapsedTime;
+			}
+
+			// rotate
+			for (int i = 0; i < vObjects[0].model->vPoints.size(); i++) {
+				float angle = O.angle * pi / 180.0f;
+				/*
+				vObjects[0].tModel.vPoints[i].dX = vObjects[0].model->vPoints[i].dX * cos(angle) - vObjects[0].model->vPoints[i].dY * sin(angle);// * fElapsedTime;
+				vObjects[0].tModel.vPoints[i].dY = vObjects[0].model->vPoints[i].dX * sin(angle) - vObjects[0].model->vPoints[i].dY * cos(angle);// * fElapsedTime;
+				*/
+			}
+
+			// render triangle
+			for (int i = 0; i < O.model->vPoints.size() - 1; i++) {
+				for (int j = i; j < O.model->vPoints.size(); j++) {					
+					DrawLine(O.tModel.vPoints[i].dX, O.tModel.vPoints[i].dY, O.tModel.vPoints[j].dX, O.tModel.vPoints[j].dY, olc::BLACK);
+				}
 			}
 		}
 
-		if (GetKey(olc::Key::X).bPressed) {
-			double scale = 1.5f;
-			for (Point &p : vModels.front().vPoints) {
-				p.dX *= scale;
-				p.dY *= scale;
+		// handle user input
+		if (GetKey(olc::Key::X).bHeld) {
+			if (vObjects[0].scale < 100.0f) {
+				vObjects[0].scale += 1.0f;
 			}
 		}
-		if (GetKey(olc::Key::Z).bPressed) {
-			double scale = 0.5f;
-			for (Point& p : vModels.front().vPoints) {
-				p.dX *= scale;
-				p.dY *= scale;
+		if (GetKey(olc::Key::Z).bHeld) {
+			if (vObjects[0].scale > 6.0f) {
+				vObjects[0].scale -= 1.0f;
 			}
+			
 		}
-
 		if (GetKey(olc::Key::RIGHT).bPressed) {
-			double angle = 15.0f * pi / 180.0f;
-			for (Point& p : vModels.front().vPoints) {
-				p.dX = p.dX * cos(angle) - p.dY * sin(angle);
-				p.dY = p.dX * sin(angle) + p.dY * cos(angle);
-			}
+			vObjects[0].angle += 15.0f;
 		}
 		if (GetKey(olc::Key::LEFT).bPressed) {
-			double angle = -15.0f * pi / 180.0f;
-			for (Point& p : vModels.front().vPoints) {
-				p.dX = p.dX * cos(angle) - p.dY * sin(angle);
-				p.dY = p.dX * sin(angle) + p.dY * cos(angle);
-			}
+			vObjects[0].angle -= 15.0f;
 		}
-
 
 		return true;
 	}
@@ -109,7 +123,7 @@ private:
 int main() {
 
 	VectorGraphic demo;
-	if (demo.Construct(256 * 4, 240 * 4, 1, 1))
+	if (demo.Construct(256 * 4, 240 * 4, 1, 1, false, false))
 		demo.Start();
 
 
